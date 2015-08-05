6d6b776525d2dc1290c76568f79b8f7dc7df4a49
Nikolay Denisenko <mail2nikolas@gmail.com>
2015-08-05 12:56:27 +0300

    annotations and fix namespace in composer

--- src/LaravelLangServiceProvider.php
+++ src/LaravelLangServiceProvider.php
@@ -6,6 +6,11 @@ use Illuminate\Support\ServiceProvider;
 
 class LaravelLangServiceProvider extends ServiceProvider
 {
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
     public function boot()
     {
         $this->publishes([__DIR__.'../resources/lang' => base_path('resources/lang')]);
