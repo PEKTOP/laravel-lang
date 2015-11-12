ef02cb69579a5da5dfd09abeacedfd8bad8280f6
Nikolay Denisenko <mail2nikolas@gmail.com>
2015-11-12 16:24:25 +0300

    add required_unless lang line

--- resources/lang/ru/validation.php
+++ resources/lang/ru/validation.php
@@ -60,6 +60,7 @@ return [
     'regex'                => 'Поле :attribute имеет ошибочный формат.',
     'required'             => 'Поле :attribute обязательно для заполнения.',
     'required_if'          => 'Поле :attribute обязательно для заполнения, когда :other равно :value.',
+    'required_unless'      => 'Поле :attribute обязательно для заполнения, когда :other не равно :values.',
     'required_with'        => 'Поле :attribute обязательно для заполнения, когда :values указано.',
     'required_with_all'    => 'Поле :attribute обязательно для заполнения, когда :values указаны.',
     'required_without'     => 'Поле :attribute обязательно для заполнения, когда :values не указано.',
